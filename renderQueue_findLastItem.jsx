7bc309658738d0c7237625e4f0e7c68e0843aa02
DanialeRia <daniele.ria@gmail.com>
2018-08-30 09:38:05 +0100

    added first 2 snippets

---
+++ renderQueue_findLastItem.jsx
@@ -0,0 +1,1 @@
+﻿myRQ = app.project.renderQueue;numItems = app.project.renderQueue.numItems;lastItemIndex = numItems;myTargetItem = myRQ.item(lastItemIndex).outputModule(1).file;alert(myTargetItem);
